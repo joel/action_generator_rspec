d23556b863a5166e41e78aa4827d26c64af57f5a
Joel AZEMAR <joel.azemar@gmail.com>
2024-05-27 12:26:10 +0200

    Initial Commit

---
+++ lib/action_generator_rspec.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "action_generator_rspec/version"
+
+module ActionGeneratorRspec
+  class Error < StandardError; end
+  # Your code goes here...
+end
