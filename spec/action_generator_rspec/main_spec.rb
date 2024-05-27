f1fb50edf3ccf18a1d7bcf0107a6d1923df04a64
Joel AZEMAR <joel.azemar@gmail.com>
2024-05-27 12:26:14 +0200

    Add Main Spec

---
+++ spec/action_generator_rspec/main_spec.rb
@@ -0,0 +1,14 @@
+module ActionGeneratorRspec
+  RSpec.describe Main do
+    subject(:foo) { described_class.new }
+    describe '#action_generator_rspec' do
+      context 'when all is good' do
+        let(:input) { 'foo' }
+        let(:result) { 'foo' }
+        it do
+          expect(foo.action_generator_rspec(input)).to eql(result)
+        end
+      end
+    end
+  end
+end
